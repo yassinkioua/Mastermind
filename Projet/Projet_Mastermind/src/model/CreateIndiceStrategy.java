4109b8455ee0b56b80e9ad9053d3740055317e5d
Yassin Kioua <yassin.kioua@etu.unistra.fr>
2023-12-10 01:02:59 +0100

    Preparation rendu 1

---
+++ Projet/Projet_Mastermind/src/model/CreateIndiceStrategy.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface CreateIndiceStrategy {
+    public Indice[] afficherIndice(Indice[] indices);
+}
---
+++ Projet/Projet_Mastermind/src/model/CreationNumerique.java
@@ -0,0 +1,8 @@
+package model;
+
+public class CreationNumerique implements CreateIndiceStrategy{
+    @Override
+    public Indice[] afficherIndice(Indice[] indices) {
+        return new Indice[0];
+    }
+}
